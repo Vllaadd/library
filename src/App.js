9ce6a925047ef40416e67d74a879eb50aa0530b9
Vllaadd <vlladdiimiir@gmail.com>
2022-11-14 04:26:02 -0500

    errors fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
-import {Link} from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <nav>
         <Link to="/home">Home</Link>
+        <br />
         <Link to="/bookshelf">Bookshelf</Link>
       </nav>
-      <Route index element={<Home />} />
-      <Route path="home" element={<Home />}/>;
-      <Route path="boogit skshelft" element={<Bookshelf/>} />
-      <Route path="*" element={<NoMatch />}/>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />;
+        <Route path="boogit skshelft" element={<Bookshelf />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </div>
   );
 }
 
-const Bookshelf = () =>{
+const NoMatch = () => {
+  return (<p>There's nothing here, you need to go back. </p>)
+}
+
+const Home = () => {
+  return (<p>Hello, this is Home page. </p>)
+}
+
+const Bookshelf = () => {
   const books = [
-    {title: "Title Road to Rust"},
-    {title: "The road to react",
-    isCompleted: true},
+    { title: "Title Road to Rust" },
+    {
+      title: "The road to react",
+      isCompleted: true
+    },
   ];
 
-return(
-  <>
-  <h2>Bookshelft</h2>
-  <ul>{books.map((book)=>(
-    <li key={book.title}>{book.title}</li>
-  ))}
-    </ul>
-  </>
-)
-  }
+  return (
+    <>
+      <h2>Bookshelft</h2>
+      <ul>{books.map((book) => (
+        <li key={book.title}>{book.title}</li>
+      ))}
+      </ul>
+    </>
+  )
+}
+
+export default App;
